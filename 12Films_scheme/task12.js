f6e760692e4399ceb3cb1948af77ab2471208239
anton <antons.zn@gmail.com>
2018-03-02 12:19:34 +0200

    update

--- 12Films_scheme/task12.js
+++ 12Films_scheme/task12.js
@@ -1,178 +1,72 @@
 //задания на уровень
 const tasks = [
- 	task1 = {task:``,
-			sec:30,
-			sizeFormatAnswer: `9rem`,
-			formatAnswer:"США/Европа",
-			pict:"./Images/01.jpg"},
-	task2 = {task:``,
- 			formatAnswer:`производство США/Европа`,
-			sec:30,
- 			pict:"./Images/02.jpg"},
-	task3 = {task:``,
-			sec:30,
-			sizeFormatAnswer: ``,
-			sizeTask:``,
-			formatAnswer:`Фильм 1987 года.`,
 
-			pict:"./Images/03.jpg"},
-	task4 = {task:``,
-			sec:30,
-			sizeFormatAnswer: ``,
-			sizeTask:``,
-			formatAnswer:``,
-
-			pict:"./Images/04.jpg"},
-	task5 = {task:``,
-			sec:30,
-			sizeFormatAnswer: ``,
-			sizeTask:``,
-			formatAnswer:`Спилберг, фильм 1993г`,
-
-			pict:"./Images/05.jpg"},
-	task6 = {task:``,
-			sec:30,
-			sizeTask:``,
-			sizeFormatAnswer: `7rem`,
-			formatAnswer:`Я очень люблю загадывть этот фильм.`,
-
-			pict:"./Images/06.jpg"},
-	task7 = {task:``,
-			sec:30,
-			sizeFormatAnswer: ``,
-			sizeTask:``,
-			formatAnswer:``,
-
-			pict:"./Images/07.jpg"},
-	
-	task9 = {task:``,
-			sec:30,
-			sizeFormatAnswer: `9rem`,
-			sizeTask:``,
-			formatAnswer:`Это гном!`,
-
-			pict:"./Images/09.jpg"},
 	task10 = {task:``,
 			sec:30,
 			sizeFormatAnswer: ``,
 			sizeTask:``,
 			formatAnswer:``,
-
 			pict:"./Images/10.jpg"},
  	task11 = {task:``,
 			sec:30,
 			formatAnswer:``,
-
 			pict:"./Images/11.jpg"},
 	task12 = {task:``,
  			sec:30,
  			formatAnswer:``,
-
  			pict:"./Images/12.jpg"},
 	task13 = {task:``,
 			sec:30,
 			sizeFormatAnswer: `10rem`,
 			sizeTask:``,
 			formatAnswer:`Фильм Марвелл.`,
-
 			pict:"./Images/13.jpg"},
 	task14 = {task:``,
 			sec:30,
 			sizeFormatAnswer: `9rem`,
 			sizeTask:``,
-			formatAnswer:`По книге Кинга.`,
-
+			formatAnswer:`Это мышь. По книге Кинга.`,
 			pict:"./Images/14.jpg"},
 	task15 = {task:``,
 			sec:30,
 			sizeFormatAnswer: ``,
 			sizeTask:``,
 			formatAnswer:``,
-
 			pict:"./Images/15.jpg"},
 	task16 = {task:``,
 			sec:30,
 			sizeFormatAnswer: ``,
 			sizeTask:``,
 			formatAnswer:``,
-
 			pict:"./Images/16.jpg"},
-
-	task20 = {task:``,
-			sec:30,
-			sizeFormatAnswer: ``,
-			sizeTask:``,
-			formatAnswer:`производство СССР/СНГ`,
-
-			pict:"./Images/20.jpg"},
- 	task21 = {task:``,
-			sec:30,
-			formatAnswer:``,
-
-			pict:"./Images/21.jpg"},
 	task22 = {task:``,
  			sec:30,
- 			formatAnswer:``,
-
+ 			formatAnswer:`производство СССР/СНГ`,
  			pict:"./Images/22.jpg"},
 	task23 = {task:``,
 			sec:30,
 			sizeFormatAnswer: ``,
 			sizeTask:``,
-			formatAnswer:``,
-
+			formatAnswer:`производство СССР/СНГ`,
 			pict:"./Images/23.jpg"},
 	task24 = {task:``,
 			sec:30,
 			sizeFormatAnswer: ``,
 			sizeTask:``,
 			formatAnswer:``,
-
 			pict:"./Images/24.jpg"},
 	task30 = {task:``,
 			sec:30,
 			sizeFormatAnswer: ``,
 			sizeTask:``,
 			formatAnswer:`производство США/Европа`,
-
-			pict:"./Images/30.jpg"}];
+			pict:"./Images/30.jpg"}
+];
 
 
 
 const answers = [
-  	task1 = {
-			sec:12, formatAnswer:"",
-			task:"Форест Гамп",
-			pict:"./Images/01.jpg"},
-	task2 = {
-			sec:12, formatAnswer:"",
- 			task:`Гарри Поттер`,
- 			pict:"./Images/02.jpg"},
-	task3 = {
-			sec:12, formatAnswer:"",
-			task:`За бортом`,
-			pict:"./Images/03.jpg"},
-	task4 = {
-			sec:12, formatAnswer:"",
-			task:`Хатико`,
-			pict:"./Images/04.jpg"},
-	task5 = {
-			sec:12, formatAnswer:"",
-			task:`Список Шиндлера`,
-			pict:"./Images/05.jpg"},
-	task6 = {
-			sec:12, formatAnswer:"",
-			task:`Хороший, Плохой, Злой`,
-			pict:"./Images/06.jpg"},
-	task7 = {
-			sec:12, formatAnswer:"",
-			task:`Начало`,
-			pict:"./Images/07.jpg"},
-	
-	task9 = {
-			sec:12, formatAnswer:"",
-			task:`Амели`,
-			pict:"./Images/09.jpg"},
+  	
 	task10 = {
 			sec:12, formatAnswer:"",
 			task:`От заката до рассвета`,
@@ -201,14 +95,6 @@ const answers = [
 			sec:12, formatAnswer:"",
 			task:`5ый элемент`,
 			pict:"./Images/16.jpg"},
-	task20 = {
-			sec:12, formatAnswer:"",
-			task:`Вий`,
-			pict:"./Images/20.jpg"},
- 	task21 = {
-			sec:12, formatAnswer:"",
-			task:`Любовь и голуби`,
-			pict:"./Images/21.jpg"},
 	task22 = {
  			sec:12, formatAnswer:"",
  			task:`Джентльмены удачи`,
@@ -219,11 +105,12 @@ const answers = [
 			pict:"./Images/23.jpg"},
 	task24 = {
 			sec:12, formatAnswer:"",
-			task:`В бой идут один старики"`,
+			task:`В бой идут один старики`,
 			pict:"./Images/24.jpg"},
 	task30 = {
 			sec:12, formatAnswer:"",
-			task:`Как приручить дракона `,
-			pict:"./Images/30.jpg"}];
+			task:`Как приручить дракона`,
+			pict:"./Images/30.jpg"}
+];
 			
 
---
+++ 13Films_kadr/task13.js
@@ -0,0 +1,106 @@
+//задания на уровень
+const tasks = [
+ 	task1 = {task:``,
+			sec:30,
+			sizeFormatAnswer: `9rem`,
+			formatAnswer:"США/Европа",
+			pict:"./Images/01.jpg"},
+	task2 = {task:``,
+ 			formatAnswer:``,
+			sec:30,
+ 			pict:"./Images/02.jpg"},
+	task3 = {task:``,
+			sec:30,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:`Комедия 1987г`,
+			pict:"./Images/03.jpg"},
+	task4 = {task:``,
+			sec:30,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:``,
+			pict:"./Images/04.jpg"},
+	task5 = {task:``,
+			sec:30,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:`Спилберг, фильм 1993г`,
+			pict:"./Images/05.jpg"},
+	task6 = {task:``,
+			sec:30,
+			sizeTask:``,
+			sizeFormatAnswer: `7rem`,
+			formatAnswer:`Опять загадываю этот фильм.`,
+			pict:"./Images/06.jpg"},
+	task7 = {task:``,
+			sec:30,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:`это юла такая`,
+			pict:"./Images/07.jpg"},
+ 	task9 = {task:``,
+			sec:30,
+			sizeFormatAnswer: `9rem`,
+			sizeTask:``,
+			formatAnswer:`Это гном!`,
+			pict:"./Images/09.jpg"},
+	task20 = {task:``,
+			sec:30,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:``,
+			pict:"./Images/20.jpg"},
+ 	task21 = {task:``,
+			sec:30,
+			formatAnswer:``,
+			pict:"./Images/21.jpg"}
+];
+	
+
+
+
+const answers = [
+  	task1 = {
+			sec:12, formatAnswer:"",
+			task:"Форест Гамп",
+			pict:"./Images/01.jpg"},
+	task2 = {
+			sec:12, formatAnswer:"",
+ 			task:`Гарри Поттер`,
+ 			pict:"./Images/02.jpg"},
+	task3 = {
+			sec:12, formatAnswer:"",
+			task:`За бортом`,
+			pict:"./Images/03.jpg"},
+	task4 = {
+			sec:12, formatAnswer:"",
+			task:`Хатико`,
+			pict:"./Images/04.jpg"},
+	task5 = {
+			sec:12, formatAnswer:"",
+			task:`Список Шиндлера`,
+			pict:"./Images/05.jpg"},
+	task6 = {
+			sec:12, formatAnswer:"",
+			task:`Хороший, Плохой, Злой`,
+			pict:"./Images/06.jpg"},
+	task7 = {
+			sec:12, formatAnswer:"",
+			task:`Начало`,
+			pict:"./Images/07.jpg"},
+	task9 = {
+			sec:12, formatAnswer:"",
+			task:`Амели`,
+			pict:"./Images/09.jpg"},
+	task20 = {
+			sec:12, formatAnswer:"",
+			task:`Вий`,
+			pict:"./Images/20.jpg"},
+ 	task21 = {
+			sec:12, formatAnswer:"",
+			task:`Любовь и голуби`,
+			pict:"./Images/21.jpg"},
+];
+			
+
